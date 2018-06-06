4402cc576d8f3d7126a2e3e3f94ce25e9e54b5b2
Michael Froehlich <m.froehlich@tum.de>
2018-06-06 14:09:25 -0700

    Add Settings menu

--- Dice/Presentation/HistoryViewController.swift
+++ Dice/Presentation/HistoryViewController.swift
@@ -10,4 +10,35 @@ import UIKit
 
 class HistoryViewController: UIViewController {
     
+    private static let showSettingSegueIdentifier = "showSettingsSegue"
+    private var expanded = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        styleView()
+    }
+    
+    private func styleView() {
+        view.layer.cornerRadius = 10
+        
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOpacity = 0.1
+        view.layer.shadowRadius = 20
+        view.layer.shadowOffset = CGSize(width: 0, height: 10.0)
+        view.layer.shouldRasterize = true
+    }
+    
+    @IBAction func settingsButtonPressed(_ sender: UIButton) {
+        
+        if expanded {
+            showSettingsScene()
+        } else {
+            // expand and then switch to settings
+        }
+    }
+    
+    private func showSettingsScene() {
+        performSegue(withIdentifier: HistoryViewController.showSettingSegueIdentifier, sender: nil)
+    }
+    
 }
--- Dice/Presentation/SettingViewController.swift
+++ Dice/Presentation/SettingViewController.swift
@@ -10,4 +10,26 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
+    @IBOutlet var containerView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        styleView()
+    }
+    
+    private func styleView() {
+        view.backgroundColor = UIColor.white.withAlphaComponent(0.0)
+        
+        containerView.layer.cornerRadius = 10
+        
+        containerView.layer.shadowColor = UIColor.black.cgColor
+        containerView.layer.shadowOpacity = 0.1
+        containerView.layer.shadowRadius = 20
+        containerView.layer.shadowOffset = CGSize(width: 0, height: 10.0)
+        containerView.layer.shouldRasterize = true
+    }
+    
+    @IBAction func closeButtonPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
