2fd306d914012f5af4ed08388e4b7648185ba345
Michael Froehlich <m.froehlich@tum.de>
2018-06-06 20:52:24 -0700

    Refactor to animate using UIViewPropertyAnimator

--- Dice/Presentation/ContainerViewController.swift
+++ Dice/Presentation/ContainerViewController.swift
@@ -16,17 +16,24 @@ class ContainerViewController: UIViewController {
     @IBOutlet var historyViewHeightConstraint: NSLayoutConstraint!
     
     private var historyViewIsOpen = false
+    private let animator = UIViewPropertyAnimator()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ContainerViewController.historyViewPressed))
-        let swipeUpGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(ContainerViewController.historyViewPressed))
-        swipeUpGestureRecognizer.direction = .up
-        let swipeDownGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(ContainerViewController.historyViewPressed))
-        swipeDownGestureRecognizer.direction = .down
-        historyViewContainer.addGestureRecognizer(swipeUpGestureRecognizer)
-        historyViewContainer.addGestureRecognizer(swipeDownGestureRecognizer)
+        let tapGestureRecongizer = UITapGestureRecognizer(target: self, action: #selector(ContainerViewController.historyViewPressed))
+        historyViewContainer.addGestureRecognizer(tapGestureRecongizer)
+        
+//        let swipeUpGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(ContainerViewController.historyViewPressed))
+//        swipeUpGestureRecognizer.direction = .up
+//        let swipeDownGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(ContainerViewController.historyViewPressed))
+//        swipeDownGestureRecognizer.direction = .down
+//        historyViewContainer.addGestureRecognizer(swipeUpGestureRecognizer)
+//        historyViewContainer.addGestureRecognizer(swipeDownGestureRecognizer)
+        
+        
+//
+//        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(ContainerViewController.handlePan))
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -56,6 +63,24 @@ class ContainerViewController: UIViewController {
         historyViewIsOpen = !historyViewIsOpen
     }
     
+    private func handlePan(recognizer: UIPanGestureRecognizer) {
+//        switch recognizer.state {
+//        case .began:
+//            animator = UIViewPropertyAnimator(duration: 3, curve: .easeOut, animations: {
+//                myView.transform = CGAffineTransform(translationX: 275, y: 0)
+//                myView.alpha = 0
+//            })
+//            animator.startAnimation()
+//            animator.pauseAnimation()
+//        case .changed:
+//            animator.fractionComplete = recognizer.translation(in: myView).x / 275
+//        case .ended:
+//            animator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
+//        default:
+//            ()
+//        }
+    }
+    
     private func animateExpandHistoryView() {
         
         var screenHeight = UIScreen.main.bounds.height
@@ -77,23 +102,26 @@ class ContainerViewController: UIViewController {
         let diceOffset = -1.0 * screenHeight * 0.5 + calculatedDiceSpace * 0.5 + dicePadding
         let historyViewHeight = screenHeight * fractionOfHeightForHistory
         
-        UIView.animate(withDuration: 0.3, animations: {
+        let transitionAnimator = UIViewPropertyAnimator(duration: 0.4, dampingRatio: 1, animations: {
             self.diceViewCenterVerticalConstraint.constant = diceOffset
             self.diceViewHeightConstraint.constant = diceHeight
             self.historyViewHeightConstraint.constant = historyViewHeight
             self.view.layoutIfNeeded()
         })
+        
+        transitionAnimator.startAnimation()
     }
     
    private func animateContractHistoryView() {
-        UIView.animate(withDuration: 0.3, animations: {
+        let transitionAnimator = UIViewPropertyAnimator(duration: 0.5, dampingRatio: 1, animations: {
             self.diceViewCenterVerticalConstraint.constant = -50.0
             self.diceViewHeightConstraint.constant = 240.0
             self.historyViewHeightConstraint.constant = 141.0
             self.view.layoutIfNeeded()
         })
+    
+        transitionAnimator.startAnimation()
     }
-
     
 }
 
